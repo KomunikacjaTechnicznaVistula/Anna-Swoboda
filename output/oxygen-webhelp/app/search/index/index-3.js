5862ecfd657b2aeaef4181ed03ef16bb66f28c27
Anna <annaswobodaa@gmail.com>
2022-03-13 11:45:10 +0100

    new files + output

---
+++ output/oxygen-webhelp/app/search/index/index-3.js
@@ -0,0 +1,4 @@
+define(function () {
+// Auto generated index for searching.
+return {"something":"0*12*a$h$t$16,1*0*8,2*30*1$3,3*0*b","table":"0*1*11$12,3*20*-1$-1$6$7","text":"0*1*c$n,1*10*-1$2"};
+});
---
+++ output/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,11 @@
+define(function() {var keywords=[{w:"my",p:["p0","p3"]},{w:"first",p:["p0","p3"]},{w:"map",p:["p0"]},{w:"concept1",p:["p1"]},{w:"f",p:["p1"]},{w:"Doing",p:["p2"]},{w:"something",p:["p2"]},{w:"reference",p:["p3"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6];
+ph["p3"]=[0, 1, 7];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
